60aee99fadf4b518e2d383f7b2769de9444ac398
rahulgautam <rahulgautam034@gmail.com>
2024-12-24 15:44:14 +0530

    spring boot, eureka server and spring cloud api gateway version update

--- src/main/java/com/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -3,6 +3,11 @@ package com.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.support.ipresolver.RemoteAddressResolver;
+import org.springframework.cloud.gateway.support.ipresolver.XForwardedRemoteAddressResolver;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +16,17 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+//	@Bean
+//	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+//		final String sbl = "http://localhost:8085";
+//
+//		return builder.routes()
+//				//gb-sbl-rest
+//				.route("common-ws", r -> r
+//						.path("/api/v1.0/flight/**")
+//						.uri(sbl)).build();
+//	}
+
 
 }
